1f4fd5c23526072e5f38d7e6dfb2cb8990cc17a9
tomaszroszko <tomaszroszko@tjsoftware.co>
2019-08-06 18:15:38 +0200

    Add pipeline to deploy changes from codecommit to s3

--- cloudformation_template/tests/test_codepipeline.py
+++ cloudformation_template/tests/test_codepipeline.py
@@ -10,3 +10,10 @@ def test_cloudformation_pipeline_template():
     Test if cloudformation_pipeline template is valid
     """
     _test_template('macros/codepipeline/cloudformation_pipeline.template')
+
+
+def test_codecommit_to_s3_template():
+    """
+    Test if codecommit_to_s3 template is valid
+    """
+    _test_template('macros/codepipeline/codecommit_to_s3.template')
