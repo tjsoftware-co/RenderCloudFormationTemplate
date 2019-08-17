9cb228304fae91b16b02ef0b23d1f82ab107ccda
tomaszroszko <tomaszroszko@tjsoftware.co>
2019-08-17 10:47:31 +0200

    S3 hosting website

--- cloudformation_template/tests/test_s3.py
+++ cloudformation_template/tests/test_s3.py
@@ -10,3 +10,17 @@ def test_bucket_template():
     Test if s3 bucket template is valid
     """
     _test_template('macros/s3/bucket.template')
+
+
+def test_website_template():
+    """
+    Test if s3 bucket that create a hosted website is valid
+    """
+    _test_template('macros/s3/website.template')
+
+
+def test_website_redirect_template():
+    """
+    Test if s3 bucket created correct website bucket that redirects all traffic to another bucket
+    """
+    _test_template('macros/s3/website_redirect.template')
