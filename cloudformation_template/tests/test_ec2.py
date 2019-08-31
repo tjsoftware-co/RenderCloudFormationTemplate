890bd66ec7ea2492160732589933832f38a2ddcf
tomaszroszko <tomaszroszko@tjsoftware.co>
2019-08-31 19:13:40 +0200

    Add vpc macros

--- cloudformation_template/tests/test_ec2.py
+++ cloudformation_template/tests/test_ec2.py
@@ -18,3 +18,30 @@ def test_container_instances_security_group():
     """
     _test_template('macros/ec2/container_instances_security_group.template')
 
+
+def test_vpc():
+    """
+    Test if vpc  template is valid
+    """
+    _test_template('macros/ec2/vpc.template')
+
+
+def test_internet_gateway():
+    """
+    Test if internet gateway template is valid
+    """
+    _test_template('macros/ec2/internet_gateway.template')
+
+
+def test_internet_gateway_attachment():
+    """
+    Test if internet gateway attachment template is valid
+    """
+    _test_template('macros/ec2/internet_gateway_attachment.template')
+
+
+def test_subnet():
+    """
+    Test if subnet template is valid
+    """
+    _test_template('macros/ec2/subnet.template')
